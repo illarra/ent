43b05f9a5b264e588f9b32a7c5ccde4952bc19fb
doup <asier@illarra.com>
2017-05-19 21:30:07 +0200

    Rename `ent-kitchen-sink` component to `ent-style-guide`

--- project/wordpress/app/themes/ent/ent/VisualComposer/components/ent-style-guide/component.php
+++ project/wordpress/app/themes/ent/ent/VisualComposer/components/ent-style-guide/component.php
@@ -1,7 +1,7 @@
 <?php
 use \Ent\VisualComposer\Field;
 
-class WPBakeryShortCode_ent_kitchen_sink extends Ent\VisualComposer\ShortCode {
+class WPBakeryShortCode_ent_style_guide extends Ent\VisualComposer\ShortCode {
     public static $is_layout = true;
 
     protected function get_context_data(array $fields) {
