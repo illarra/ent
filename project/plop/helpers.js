cd4a184bc7025563d07aa72e0b28df52e6225025
doup <asier@illarra.com>
2017-05-30 12:57:20 +0200

    Add `post-type` generators

--- project/plop/helpers.js
+++ project/plop/helpers.js
@@ -1,9 +1,18 @@
 var crypto = require('crypto');
 var changeCase = require('change-case');
+var slug = require('slug');
 
 module.exports = function (plop) {
     plop.addHelper('concat', function (...args) {
         return args.splice(0, args.length - 1).join('');
+    });
+
+    plop.addHelper('phpBool', function (value) {
+        return value ? 'true' : 'false';
+    });
+
+    plop.addHelper('phpArray', function (value) {
+        return JSON.stringify(value).replace(/","/g, '", "').replace(/"/g, "'");
     });
 
     plop.addHelper('properSnakeCase', function (str) {
@@ -12,5 +21,13 @@ module.exports = function (plop) {
 
     plop.addHelper('randomKey', function () {
         return crypto.randomBytes(32).toString('hex');
+    });
+
+    plop.addHelper('slug', function (str) {
+        return slug(str, { lower: true });
+    });
+
+    plop.addHelper('ucFirst', function (str) {
+        return changeCase.ucFirst(str);
     });
 };
--- project/plopfile.js
+++ project/plopfile.js
@@ -16,6 +16,59 @@ module.exports = function (plop) {
         actions: [],
     });
 
+    plop.setGenerator('post-type', {
+        description: 'WordPress post type',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'Name:',
+            validate: validate(['required', 'Name is required.']),
+        }, {
+            type: 'input',
+            name: 'plural',
+            message: 'Plural label:',
+            validate: validate(['required', '"Plural label" is required.']),
+        }, {
+            type: 'input',
+            name: 'singular',
+            message: 'Singular label:',
+            validate: validate(['required', '"Singular label" is required.']),
+        }, {
+            type: 'confirm',
+            name: 'enable_vc',
+            message: 'Enable VisualComposer:',
+            default: false,
+        }, {
+            type: 'checkbox',
+            name: 'supports',
+            message: 'Select supported post type features:',
+            // Avaliable supports: 'title', 'editor', 'comments', 'revisions', 'trackbacks', 'author', 'excerpt', 'page-attributes', 'thumbnail', 'custom-fields', and 'post-formats'
+            // Default supports: 'title', 'editor'
+            choices: [
+                { name: 'title',     checked: true },
+                { name: 'editor',    checked: true },
+                { name: 'thumbnail', checked: false },
+                { name: 'excerpt',   checked: false },
+                { name: 'revisions', checked: false },
+            ]
+        }],
+        actions: [{
+            type: 'add',
+            path: 'wordpress/app/themes/ent/src/post-types/{{ dashCase name }}.php',
+            templateFile: 'plop/post-type/post-type.hbs'
+        }],
+    });
+
+    plop.setGenerator('post-type:news', {
+        description: 'WordPress News post type. Useful when blog posts and news need to be separated.',
+        prompts: [],
+        actions: [{
+            type: 'add',
+            path: 'wordpress/app/themes/ent/src/post-types/news.php',
+            templateFile: 'plop/post-type-news/news.hbs'
+        }]
+    });
+
     plop.setGenerator('widget', {
         description: 'WordPress widget',
         prompts: [{
