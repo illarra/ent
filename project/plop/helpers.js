3d07158f5f048725e6768aa84ed4b8fc15d91609
doup <asier@illarra.com>
2017-05-30 10:49:23 +0200

    Add `properSnakeCase` plop helper

--- project/plop/helpers.js
+++ project/plop/helpers.js
@@ -1,8 +1,13 @@
 var crypto = require('crypto');
+var changeCase = require('change-case');
 
 module.exports = function (plop) {
-	plop.addHelper('concat', function (...args) {
+    plop.addHelper('concat', function (...args) {
         return args.splice(0, args.length - 1).join('');
+    });
+
+    plop.addHelper('properSnakeCase', function (str) {
+        return changeCase.snake(str).split('_').map(changeCase.ucFirst).join('_');
     });
 
     plop.addHelper('randomKey', function () {
