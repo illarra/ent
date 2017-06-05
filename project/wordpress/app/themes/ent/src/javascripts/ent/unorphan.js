bb3903a93388f9519f3f04a7075796db658ac42b
doup <asier@illarra.com>
2017-06-05 20:11:10 +0200

    Tweak unorphan js

--- project/wordpress/app/themes/ent/src/javascripts/ent/unorphan.js
+++ project/wordpress/app/themes/ent/src/javascripts/ent/unorphan.js
@@ -1,6 +1,6 @@
 var $           = require('jquery');
 var unorphan    = require('unorphan');
-var selector    = 'h1, h2, h3, h4, h5, h6, blockquote, [data-unorphan]';
+var selector    = 'h1, h2, h3, h4, h5, h6, blockquote, [data-unorphan], [data-unorphan] *';
 var notSelector = '[data-dont-unorphan]';
 
 // Unorphan en medium o más
